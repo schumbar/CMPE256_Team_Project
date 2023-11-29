621c456972b00eb267b49747478bfe9b9202e03d
Shawn Chumbar <schumbar@ucsc.edu>
2023-11-28 19:57:37 -0800

    updated backend and frontend

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import script_272 # Replace with your actual script name
+import summarizer
 
 app = Flask(__name__)
 
@@ -10,8 +10,23 @@ def index():
 @app.route("/get", methods=["GET", "POST"])
 def submit_query():
     user_query = request.form['user_query']
-    response = script_272.get_response(user_query)  # Replace with the actual function call
+    response = summarizer.get_response(user_query)
     return render_template('response.html', response=response)
 
+@app.route('/generate_summary', methods=['POST'])
+def generate_summary():
+    # Retrieve data from request
+    summary_length = request.form['length']
+    if summary_length == 'short':
+        summary_length = "using 1-3 sentences"
+    elif summary_length == 'medium':
+        summary_length = "using 3-6 sentences"
+    elif summary_length == 'long':
+        summary_length = "using 6-10 sentences"
+    response = summarizer.get_response("Generate a summary of the text " + summary_length + ".")
+    return response
+    # return render_template('response.html', response=response)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    generate_summary()
